5d5fa3fffb74624ee4c8a9dcd66ebe0076576180
Frederick Gnodtke <frederick@gnodtke.net>
2018-01-27 15:55:52 +0100

    Add variety to grass

--- src/states/state-boot.ts
+++ src/states/state-boot.ts
@@ -7,8 +7,8 @@ export class StateBoot extends State {
         this.game.load.atlas("pidgeon", "/assets/pidgeon.png", "/assets/pidgeon.json");
         this.game.load.atlas("seagull", "/assets/seagull.png", "/assets/seagull.json");
         this.game.load.atlas("tower", "/assets/tower.png", "/assets/tower.json");
-        this.game.load.image("grass", "/assets/grass.png");
-        this.game.load.image("weed", "/assets/weed.png");
+        this.game.load.atlas("grass", "/assets/grass.png", "/assets/grass.json");
+        this.game.load.atlas("weed", "/assets/weed.png", "/assets/weed.json");
     }
 
     public create() {
