46600cbaa24cd25f4b602e28ba376e97a62fab2b
Frederick Gnodtke <frederick@gnodtke.net>
2018-01-27 12:54:33 +0100

    Add weed chance to tile

--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,3 @@
 export const STAMINA_PER_SECOND = 5;
 export const MAX_STAMINA = 15;
+export const WEED_CHANCE = 0.2;
--- src/tile.ts
+++ src/tile.ts
@@ -1,9 +1,15 @@
 import Victor = require("victor");
+import { RandomSeed, create as gen } from "random-seed";
+import { WEED_CHANCE } from "./const";
 
 export class Tile {
     private pos: Victor;
+    private rnd: RandomSeed;
+    private hasWeed: boolean;
 
     constructor(pos: Victor) {
         this.pos = pos;
+        this.rnd = gen(`${pos.x},${pos.y}`);
+        this.hasWeed = this.rnd.random() > WEED_CHANCE;
     }
 }
